676e6932bbfee96fd1cde4f1d94398d90bafbdca
surajpaikekar <sbppaikekar@gmail.com>
2023-12-13 15:14:38 +0530

    secret key changed in deployment.py

--- project1/project1/deployment.py
+++ project1/project1/deployment.py
@@ -2,6 +2,9 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
 CSRF_TRUSTED_ORIGINS = ['https://' +os.environ["WEBSITE_HOSTNAME"]]
 DEBUG = False
